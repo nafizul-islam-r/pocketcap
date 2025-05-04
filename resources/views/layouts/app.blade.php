501a56d8ec0a00ea833e2c147790e871fd1b2320
nafizul-islam-r <smnafizulislam@gmail.com>
2025-05-04 15:27:32 +0600

    header,footer and app layout initialized

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,23 +14,15 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="bg-white text-gray-900">
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        @include('partials.header')
+    
+        <main>
+            @yield('content')
+        </main>
+    
+        @include('partials.footer')
+    
     </body>
 </html>
