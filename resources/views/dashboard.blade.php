d602c5b92aedace2445336bf382c9f4a7022082b
Trafalgar-D-ACID <smnafizulislam@gmail.com>
2025-05-06 23:08:58 +0600

    site titles fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,4 @@
+<title>@yield('title', config('app.name') . ' | User Dashboard')</title>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
