985d848b73394d455abe9daad24e8f86c3ddbb85
Trafalgar-D-ACID <smnafizulislam@gmail.com>
2025-05-20 20:02:55 +0600

    footer fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -340,6 +340,26 @@
             </form>
         </div>
     </section>
-    @extends('partials.footer')
+
+    <footer class="w-full bg-gray-900 text-white border-t border-gray-700 pt-2 -mb-5">
+        <div
+            class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-between items-center">
+            <!-- Left Column -->
+            <div class="flex items-center space-x-2 text-sm text-gray-400">
+                <span>© 2025 PocketCap. All rights reserved. Designed and Developed by</span>
+                <a href="https://nafizul-islam-r.github.io/portfolio" class="inline-flex items-center space-x-1">
+                    <img src="{{ asset('branding.png') }}" alt="Branding Logo" width="20" class="inline-block">
+                    <span class="sr-only">Developer Logo</span>
+                </a>
+            </div>
+
+            <!-- Right Column -->
+            <div class="mt-3 sm:mt-0 flex space-x-4 text-sm text-gray-400">
+                <a href="#" class="hover:underline">Privacy Policy</a>
+                <a href="#" class="hover:underline">Terms & Conditions</a>
+            </div>
+        </div>
+    </footer>
+
 </body>
 </html>
