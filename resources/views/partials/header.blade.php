4e3a919b6fb48e64e7b444d95414c49642d66bae
Trafalgar-D-ACID <smnafizulislam@gmail.com>
2025-05-21 22:10:41 +0600

    minor issue

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -29,26 +29,27 @@
         </div>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             @if (Route::has('login') && Auth::check())
-                @if (Auth::user()->hasRole(''))
-                    <a href="{{ url('/dashboard') }}" <button type="button"
-                        class="text-white bg-blue-600 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-orange-400 dark:focus:ring-blue-800">Dashboard
-                        </button>
-                    </a>
-                @endif
                 @if (Auth::user()->hasRole('admin'))
-                    <a href="{{ url('/admin/dashboard') }}" <button type="button"
+                    <a href="{{ url('/admin/dashboard') }}"> 
+                    <button type="button"
                         class="text-white bg-blue-600 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-orange-400 dark:focus:ring-blue-800">Dashboard
                         </button>
                     </a>
-                @endif
-                @if (Auth::user()->hasRole('candidate'))
-                    <a href="{{ url('/candidate/dashboard') }}" <button type="button"
+                @elseif (Auth::user()->hasRole('candidate'))
+                    <a href="{{ url('/candidate-profiles/dashboard') }}"> 
+                    <button type="button"
                         class="text-white bg-blue-600 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-orange-400 dark:focus:ring-blue-800">Dashboard
                         </button>
                     </a>
-                @endif
-                @if (Auth::user()->hasRole('investor'))
-                    <a href="{{ url('/investor/dashboard') }}" <button type="button"
+                @elseif (Auth::user()->hasRole('investor'))
+                    <a href="{{ url('/investor-profiles/dashboard') }}"> 
+                    <button type="button"
+                        class="text-white bg-blue-600 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-orange-400 dark:focus:ring-blue-800">Dashboard
+                        </button>
+                    </a>
+                @else
+                    <a href="{{ url('/dashboard') }}">
+                    <button type="button"
                         class="text-white bg-blue-600 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-orange-400 dark:focus:ring-blue-800">Dashboard
                         </button>
                     </a>
